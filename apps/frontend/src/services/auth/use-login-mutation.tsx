e19f54293e43aeb0803ab3ab3c2171fb6b2d177c
amjadhossain1 <amjad.rabby1@gmail.com>
2024-09-23 14:11:08 +0600

    login mutation refactor

--- apps/frontend/src/services/auth/use-login-mutation.tsx
+++ apps/frontend/src/services/auth/use-login-mutation.tsx
@@ -2,27 +2,29 @@ import { useMutation } from '@tanstack/react-query';
 import { useSnackbar } from 'notistack';
 import { useCallback } from 'react';
 import { JwtReturnType } from 'src/types';
-import { PENSTRIKE_USER_KEY } from 'src/config-global';
 import { useAxios } from '../use-axios';
 import { ENTITY } from './entity';
 
 export const useLoginMutation = () => {
-  // const { update } = useLocalStorage(PENSTRIKE_USER_KEY, null);
   const { axios } = useAxios();
   const { enqueueSnackbar } = useSnackbar();
 
   const mutationFn = useCallback(
     async (params: { email: string; password: string }) =>
-      axios.post(`${ENTITY}/login`, params, { timeout: 10000 }),
+      axios.post<JwtReturnType>(`${ENTITY}/login`, params, { timeout: 10000 }),
     [axios]
   );
 
   return useMutation({
     mutationFn,
     onSuccess: (res) => {
-      // update(PENSTRIKE_USER_KEY, res.data);
       enqueueSnackbar('Email Login successful!', {
         variant: 'success',
+      });
+    },
+    onError: () => {
+      enqueueSnackbar('Email Login failed!', {
+        variant: 'error',
       });
     },
   });
--- apps/frontend/src/utils/index.ts
+++ apps/frontend/src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './design-constant';
+export * from './jwt-session';
