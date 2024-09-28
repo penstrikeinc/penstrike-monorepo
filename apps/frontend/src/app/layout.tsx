fcd3875cf9161652e454364f5e093ba28d1badee
amjadhossain1 <amjad.rabby1@gmail.com>
2024-09-29 01:45:18 +0600

    commented code removed

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -6,8 +6,6 @@ import 'simplebar-react/dist/simplebar.min.css';
 // image
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-// ----------------------------------------------------------------------
-
 // theme
 import ThemeProvider from 'src/theme';
 import { primaryFont } from 'src/theme/typography';
@@ -18,64 +16,13 @@ import { SettingsProvider, SettingsDrawer } from 'src/components/settings';
 import { AuthConsumer, AuthProvider } from 'src/auth';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// ----------------------------------------------------------------------
-
-// export const metadata = {
-//   title: 'Minimal UI Kit',
-//   description:
-//     'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
-//   keywords: 'react,material,kit,application,dashboard,admin,template',
-//   themeColor: '#000000',
-//   manifest: '/manifest.json',
-//   viewport: {
-//     width: 'device-width',
-//     initialScale: 1,
-//     maximumScale: 1,
-//   },
-//   icons: [
-//     {
-//       rel: 'icon',
-//       url: '/favicon/favicon.ico',
-//     },
-//     {
-//       rel: 'icon',
-//       type: 'image/png',
-//       sizes: '16x16',
-//       url: '/favicon/favicon-16x16.png',
-//     },
-//     {
-//       rel: 'icon',
-//       type: 'image/png',
-//       sizes: '32x32',
-//       url: '/favicon/favicon-32x32.png',
-//     },
-//     {
-//       rel: 'apple-touch-icon',
-//       sizes: '180x180',
-//       url: '/favicon/apple-touch-icon.png',
-//     },
-//   ],
-// };
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-  // Todo refactor
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      mutations: {
-        onError: (error) => console.log(error),
-        onSuccess: (data) => console.log(data),
-      },
-      queries: {
-        staleTime: 1000 * 60 * 5, // Data remains fresh for 5 minutes
-        // cacheTime: 1000 * 60 * 10, // Cached data stays for 10 minutes
-        refetchOnWindowFocus: false, // Prevent refetching on window focus
-      },
-    },
-  });
+  const queryClient = new QueryClient();
 
   return (
     <html lang="en" className={primaryFont.className}>
