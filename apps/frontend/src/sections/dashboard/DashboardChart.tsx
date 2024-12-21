2eb0bc6d5d9f7ef8cbf4b27f4d503c5ffcb02577
amjadhossain1 <amjad.rabby1@gmail.com>
2024-12-22 01:14:57 +0600

    dashboard chart page improvments

--- apps/frontend/src/sections/dashboard/DashboardChart.tsx
+++ apps/frontend/src/sections/dashboard/DashboardChart.tsx
@@ -1,101 +1,103 @@
-/* eslint-disable no-constant-condition */
-/* eslint-disable no-nested-ternary */
-/* eslint-disable import/no-extraneous-dependencies */
+'use client';
 
-import { Box, Button, Card, CardActions, CardContent, Container, Grid, Paper, Typography } from "@mui/material";
-// import { useSettingsContext } from "src/components/settings";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
-import ArrowDropUp from "@mui/icons-material/ArrowDropUp";
-import { alignProperty } from "@mui/material/styles/cssUtils";
-
-
-
+import { Container, Grid, Paper, Typography } from '@mui/material';
+import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md';
+import { useSettingsContext } from 'src/components/settings';
 
 const items = [
   {
-    type: "Critical",
+    type: 'Critical',
     previousWeek: 12,
-    value: 40
+    value: 40,
   },
   {
-    type: "High",
+    type: 'High',
     previousWeek: 18,
-    value: 52
+    value: 52,
   },
   {
-    type: "Medium",
+    type: 'Medium',
     previousWeek: 24,
-    value: 18
+    value: 18,
   },
   {
-    type: "Low",
+    type: 'Low',
     previousWeek: 8,
-    value: 10
+    value: 10,
   },
-]
-
-
-
+];
 
 export function DashboardChart() {
-  //   const settings = useSettingsContext();
-
+  const settings = useSettingsContext();
 
   return (
-
-    <Container>
+    <Container maxWidth={settings.themeStretch ? false : 'xl'}>
       <Grid container spacing={3}>
         {/* Loop through items */}
         {items.map((item, index) => (
           <Grid item xs={12} sm={6} md={3} key={index}>
-            <Paper elevation={3} style={{ padding: "16px", textAlign: "center" }}>
+            <Paper elevation={3} style={{ padding: '16px', textAlign: 'center' }}>
               <Typography variant="h5" component="h3" gutterBottom>
-                <Typography variant="h5" component="span" sx={{ color:  item.type === "Critical" ? "error.main": item.type === "Low"? "success.main" : item.type === "Medium" || "High"? "warning.main": "text.primary"  , fontWeight: "bold" }}>{item.type}</Typography> Vulnerabilities
-              </Typography>
-              <Container component="section" sx={{ display: "flex", justifyContent: "center" , alignItems: "center"}}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
                 <Typography
-                  variant="h3"
-                  color={
-                    item.type === "Critical"
-                      ? "error.main"
-                      : item.type === "Low"
-                      ? "success.main"
-                      : item.type === "Medium" || "High"
-                      ? "warning.main"
-                      : "text.primary"  
-                  }                >
-                  {item.value}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Paper>
-                  hellooooooo
-                </Paper>
-              </Grid>
-            </Grid>
-          </Container>
-
-
+                  variant="h5"
+                  component="span"
+                  sx={{
+                    // color:
+                    //   item.type === 'Critical'
+                    //     ? 'error.main'
+                    //     : item.type === 'Low'
+                    //       ? 'success.main'
+                    //       : item.type === 'Medium' || 'High'
+                    //         ? 'warning.main'
+                    //         : 'text.primary',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {item.type}
+                </Typography>{' '}
+                Vulnerabilities
+              </Typography>
+              <Container
+                component="section"
+                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h3"
+                      // color={
+                      //   item.type === 'Critical'
+                      //     ? 'error.main'
+                      //     : item.type === 'Low'
+                      //       ? 'success.main'
+                      //       : item.type === 'Medium' || 'High'
+                      //         ? 'warning.main'
+                      //         : 'text.primary'
+                      // }
+                    >
+                      {item.value}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Paper>hellooooooo</Paper>
+                  </Grid>
+                </Grid>
+              </Container>
 
               <Typography
                 variant="body2"
-                color={item.previousWeek >= 0 ? "success.main" : "error.main"}
+                color={item.previousWeek >= 0 ? 'success.main' : 'error.main'}
                 display="flex"
                 alignItems="center"
                 justifyContent="center"
               >
                 {item.previousWeek >= 0 ? (
-                  <ArrowDropUp fontSize="small" style={{ marginRight: 4 }} />
+                  <MdArrowDropUp fontSize="small" style={{ marginRight: 4 }} />
                 ) : (
-                  <ArrowDropDown fontSize="small" style={{ marginRight: 4 }} />
+                  <MdArrowDropDown fontSize="small" style={{ marginRight: 4 }} />
                 )}
                 {Math.abs(item.previousWeek)}% from previous week
               </Typography>
-
-
             </Paper>
           </Grid>
         ))}
@@ -103,5 +105,3 @@ export function DashboardChart() {
     </Container>
   );
 }
-
-
